2da568cafce1ae277d97203020a8120e25373c72
cnscheihing <cnscheihing@uc.cl>
2020-10-23 17:31:30 -0300

    🐛  delay fix

--- hito2/script.js
+++ hito2/script.js
@@ -1,4 +1,4 @@
-// DATA PARSING/\./g
+// DATA PARSING
 let id = 0;
 const dataParsing = (data) => {
   id++;
@@ -24,8 +24,8 @@ const dataParsing = (data) => {
 )}
 
 // CONSTANT VARIABLES: DIMENSIONS
-const CARD_HEIGHT = 300;
-const CARD_WIDTH = (5/6) * CARD_HEIGHT;
+const CARD_WIDTH = 200;
+const CARD_HEIGHT = 1.618 * CARD_WIDTH;
 const CHART_CENTER_X =  CARD_WIDTH / 2;
 const CHART_CENTER_Y =  (2/3) * CARD_HEIGHT;
 const MAX_RADIUS = (1/4) * CARD_HEIGHT;
@@ -71,91 +71,80 @@ const chartColor = position => {
   else return "red"; 
 };
 
-const generateInfo = (player) => {
-  const svg =  d3.select(`#${player.id}_svg`);
-  const g = svg.append("g").attr("id", `${player.id}_svg`);
-
-  g.selectAll("text")
-    .data(player.info)
-    .join( enter => enter.append("text") )
-    .text(d => d.value)
-    .attr("class", d => d.key)
-    .attr("y", (_, i) => 15 * (i + 1));
-}
-
-const generateChart = (player) => {
-  const svg =  d3.select(`#${player.id}_svg`);
-  const g = svg.append("g").attr("id", `${player.id}_chart`);
-  const statKeys = Object.keys(player.stats);
-
-  // background circle
-  g.append("circle")
-    .attr("cx", CHART_CENTER_X)
-    .attr("cy", CHART_CENTER_Y)
-    .attr("r", MAX_RADIUS + 10)
-    .attr("fill", "white");
-
-  // stats polygon
-  g.append("path")
-    .attr("d", getSpiderPath(Object.values(player.stats)))
-    .attr("fill", chartColor(player.position));
-  
-  // stat names
-  g.selectAll("text")
-    .data(statKeys)
-    .join(
-      enter => {
-        // labels
-        enter.append("text")
-          .attr("x", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).x)
-          .attr("y", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).y)
-          .text(d => d)
-          .style("font-size", 8);
-        
-        // outer line
-        enter.append("line")
-          .attr("x1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).x)
-          .attr("y1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).y)
-          .attr("x2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 1), 99).x)
-          .attr("y2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 1), 99).y)
-          .attr("stroke", "grey");
-        
-        // axes
-        enter.append("line")
-          .attr("x1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).x)
-          .attr("y1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).y)
-          .attr("x2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 3), 99).x)
-          .attr("y2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 3), 99).y)
-          .attr("stroke", "grey");
-      }
-    );
-}
-
-const cardContainerDrawer = (dataArray) => {
+const cardDrawer = (dataArray) => {
   d3.select("#viz-container")
     .selectAll("div")
     .data(dataArray)
     .join(
-      enter => enter.append("div"),
+      enter => {
+        const svg = enter.append("div")
+            .attr("id", (d) => (`${d.id}_div`))
+            .attr("class", "card-container")
+            .append("svg")
+            .attr("id", (d) => (`${d.id}_svg`))
+            .attr("width", CARD_WIDTH)
+            .attr("height", CARD_HEIGHT)
+            .style("background-color", d => cardColor(d.rating));
+        
+        svg.append("g")
+            .attr("id", (d) => `${d.id}_info`)
+            .selectAll("text")
+            .data((d) => d.info)
+            .join( enter => enter.append("text") )
+            .text(d => d.value)
+            .attr("class", d => d.key)
+            .attr("y", (_, i) => 15 * (i + 1));
+        
+        const gChart = svg.append("g").attr("id", (d) => `${d.id}_chart`);
+        // background circle
+        gChart.append("circle")
+          .attr("cx", CHART_CENTER_X)
+          .attr("cy", CHART_CENTER_Y)
+          .attr("r", MAX_RADIUS + 10)
+          .attr("fill", "white");
+
+        // stats polygon
+        gChart.append("path")
+          .attr("d", d => getSpiderPath(Object.values(d.stats)))
+          .attr("fill", d => chartColor(d.position));
+
+        // stat names
+        gChart.selectAll("text")
+          .data(d => Object.keys(d.stats))
+          .join(
+            enter => {
+              // labels
+              enter.append("text")
+                .attr("x", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).x)
+                .attr("y", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).y)
+                .text(d => d)
+                .style("font-size", 8);
+              
+              // outer line
+              enter.append("line")
+                .attr("x1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).x)
+                .attr("y1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).y)
+                .attr("x2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 1), 99).x)
+                .attr("y2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 1), 99).y)
+                .attr("stroke", "grey");
+              
+              // axes
+              enter.append("line")
+                .attr("x1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).x)
+                .attr("y1", (_, i) => getPolarCoordinates(getAngleFromIndex(i), 99).y)
+                .attr("x2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 3), 99).x)
+                .attr("y2", (_, i) => getPolarCoordinates(getAngleFromIndex(i + 3), 99).y)
+                .attr("stroke", "grey");
+            }
+          );     
+      },
       update => update,
       exit => exit.remove(),
-    )
-    .attr("id", (d) => (`${d.id}_div`))
-    .attr("class", "card-container")
-    .append("svg")
-    .attr("id", (d) => (`${d.id}_svg`))
-    .attr("width", CARD_WIDTH)
-    .attr("height", CARD_HEIGHT)
-    .style("background-color", d => cardColor(d.rating));
+    );
 }
-
 
 d3.csv("fifa_20_data.csv", dataParsing)
   .then((data) => {
-    cardContainerDrawer(data);
-    data.forEach(player => {
-      generateInfo(player);
-      generateChart(player);
-    });
+    cardDrawer(data);
   })
   .catch((error) => console.log(error));
