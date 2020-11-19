b90d545645ea1b60b0ef705a0ba0c1a5da65d7af
cnscheihing <cnscheihing@uc.cl>
2020-11-19 16:21:53 -0300

    🚧  partially working viz uwu

--- hito3/map.js
+++ hito3/map.js
@@ -1,5 +1,5 @@
 // CONSTANTS
-const MAP_HEIGHT = 600;
+const MAP_HEIGHT = 500;
 const MAP_WIDTH = 400;
 
 // MAP TOOLTIP
@@ -55,17 +55,25 @@ const mapDrawer = async (mapData, infoData) => {
                  .attr("width", MAP_WIDTH)
                  .attr("id", "map-svg");
 
-  const colorScale  = d3.scaleSequential().domain([
-      d3.min(infoData, d => (d.totalPopulation)),
+  const colorScale = d3.scaleSequential().domain([
+      0,
+      // d3.min(infoData, d => (d.totalPopulation)),
       d3.max(infoData, d => (d.totalPopulation))])
-    .interpolator(d3.interpolateYlGn)
+    .interpolator(d3.interpolateBlues)
+  
+  // const colorScale = d3.scaleQuantize()
+  //   .domain([0, d3.max(infoData, d => (d.totalPopulation))])
+  //   .range([colorScale2(d3.max(infoData, d => (d.totalPopulation))*1/5),
+  //     colorScale2(d3.max(infoData, d => (d.totalPopulation))*2/5),
+  //     colorScale2(d3.max(infoData, d => (d.totalPopulation))*3/5),
+  //     colorScale2(d3.max(infoData, d => (d.totalPopulation))*4/5),
+  //     colorScale2(d3.max(infoData, d => (d.totalPopulation))*5/5)])
     
   const mapProjection = d3.geoMercator().fitSize([MAP_WIDTH, MAP_HEIGHT], mapData);
   const geoPathGenerator = d3.geoPath().projection(mapProjection);
   mapSvg
     .append("g")
     .attr("id", "map-g")
-    .attr("stroke", "black")
     .selectAll("path")
     .data(mapData.features)
     .enter()
@@ -86,21 +94,21 @@ const mapDrawer = async (mapData, infoData) => {
     .on("click", function(_, d) {
       if(d3.select(this).attr("selected") == "false") {
         d3.select(this).attr("selected", "true");
-        d3.select(this).attr("fill", "red");
-        d3.select(`#p-${d.id}`).attr("fill", "red");
+        d3.select(this).attr("fill", "#f55c41");
+        d3.select(`#p-${d.id}`).attr("fill", "#f55c41");
       } else {
         d3.select(this).attr("selected", "false");
         d3.select(this).attr("fill", (d) =>
           colorScale(infoData.filter(infoDatum => infoDatum.id == d.id)[0].totalPopulation)
         );
-        d3.select(`#p-${d.id}`).attr("fill", "black");
+        d3.select(`#p-${d.id}`).attr("fill", "#123b75");
       }
     });
 
   mapSvg.append("rect")
         .attr("height", MAP_HEIGHT)
         .attr("width", MAP_WIDTH)
-        .attr("stroke", "black")
+        .attr("stroke", "#123b75")
         .attr("fill", "none");
 
   mapSvg.call(zoomMap);
@@ -123,27 +131,7 @@ d3.select("body")
   .attr("id", "plot-tooltip")
   .append("p")
   .attr("id", "plot-tooltip-name");
-
-// const plotZoom = d3
-//   .zoom()
-//   .extent([
-//     [0, 0],
-//     [width, height],
-//   ])
-//   .translateExtent([
-//     [0, 0],
-//     [width, height],
-//   ])
-//   .scaleExtent([1, 4])
-//   .on("zoom", manejadorZoom);
-
-// const plotZoomHandler = (event) => {
-//   const escalaX2 = event.transform.rescaleX(escalaX);
-//   const escalaY2 = event.transform.rescaleY(escalaY);
-//   contenedorEjeX.call(ejeX.scale(escalaX2));
-//   contenedorEjeY.call(ejeY.scale(escalaY2));
-// };
-
+  
 const scatterPlotDrawer = async (infoData) => {
   const plotSvg = d3
     .select("#plot-container")
@@ -151,6 +139,16 @@ const scatterPlotDrawer = async (infoData) => {
     .attr("height", PLOT_HEIGHT)
     .attr("width", PLOT_WIDTH)
     .attr("id", "plot-svg");
+
+  plotSvg
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", PLOT_WIDTH - PLOT_MARGIN.right - PLOT_MARGIN.left)
+    .attr("height", PLOT_HEIGHT - PLOT_MARGIN.top - PLOT_MARGIN.bottom);
+    // .attr("transform", `translate(${PLOT_MARGIN.left} ${PLOT_MARGIN.top})`);
+    // .attr("x", PLOT_MARGIN.left)
+    // .attr("y", PLOT_MARGIN.top);
 
   // SCALES
   const scaleX = d3.scaleLinear()
@@ -161,23 +159,28 @@ const scatterPlotDrawer = async (infoData) => {
     .domain([0, d3.max(infoData, d => d.depJuIndex)])
     .range([PLOT_HEIGHT - PLOT_MARGIN.bottom - PLOT_MARGIN.top, 0]);
 
+  // AXIS
+  const xAxis = d3.axisBottom(scaleX);
+  const yAxis = d3.axisLeft(scaleY);
+
   // CONTAINERS
   const pointsG = plotSvg
     .append("g")
-    .attr("transform", `translate(${PLOT_MARGIN.left} ${PLOT_MARGIN.top})`);
+    .attr("transform", `translate(${PLOT_MARGIN.left} ${PLOT_MARGIN.top})`)
+    .attr("clip-path", "url(#clip)")
   
   const xAxisG = plotSvg
     .append("g")
     .attr("transform", `translate(${PLOT_MARGIN.left} ${PLOT_HEIGHT - PLOT_MARGIN.top})`)
-    .call(d3.axisBottom(scaleX));
+    .call(xAxis);
 
   const yAxisG = plotSvg
     .append("g")
     .attr("transform", `translate(${PLOT_MARGIN.left} ${PLOT_MARGIN.top})`)
-    .call(d3.axisLeft(scaleY));
-  
+    .call(yAxis);
+
   // POINTS
-  pointsG
+  const points = pointsG
     .selectAll("circle")
     .data(infoData)
     .join(
@@ -185,10 +188,11 @@ const scatterPlotDrawer = async (infoData) => {
       update => update,
       exit => exit.remove(),
     )
-    .attr("r", 3)
+    .attr("r", 2.5)
     .attr("cx", (d) => scaleX(d.womenRate))
     .attr("cy", (d) => scaleY(d.depJuIndex))
     .attr("id", (d) => `p-${d.id}`)
+    .attr("fill", "#123b75")
     .on("mouseenter", function (event, d) {
       d3.select(this).attr("opacity", "0.5");
       showTooltip(event, "plot", d.comunaName);
@@ -198,5 +202,29 @@ const scatterPlotDrawer = async (infoData) => {
       hideTooltip("plot");
     });
 
-  // plotSvg.call(zoom);
+  // ZOOM
+  const plotZoomHandler = (event) => {
+    pointsG.attr("transform", event.transform);
+    const scaleX2 = event.transform.rescaleX(scaleX);
+    const scaleY2 = event.transform.rescaleY(scaleY);
+    points.attr("cx", (d) => scaleX2(d.womenRate));
+    points.attr("cy", (d) => scaleY2(d.depJuIndex));
+    xAxisG.call(xAxis.scale(scaleX2));
+    yAxisG.call(yAxis.scale(scaleY2));
+  };
+
+  const plotZoom = d3
+    .zoom()
+    .extent([
+      [0, 0],
+      [PLOT_WIDTH, PLOT_HEIGHT],
+    ])
+    .translateExtent([
+      [0, 0],
+      [PLOT_WIDTH, PLOT_HEIGHT],
+    ])
+    .scaleExtent([1, 4])
+    .on("zoom", plotZoomHandler);
+
+  plotSvg.call(plotZoom);
 };
